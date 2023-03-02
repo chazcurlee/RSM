8f41d372c5d0d1c1238a85d38189b8289517d86f
chazcurlee <chaz.curlee@gmail.com>
2023-03-02 11:42:49 -0500

    check up

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -6,74 +6,18 @@ const Landing = () => {
       className={`landing-container height-100prcnt grid-display grid-col-full grid-row-auto`}
     >
       <div
-        className={`text-title-container grid-col-start-2 grid-display grid-row-2 width-auto margin-bottom`}
+        className={`text-title-container grid-col-start-2 grid-display grid-row-start-2 width-auto margin-bottom`}
       >
         <div className="long-bottom flex-display flex-col justify-center grid-row-start-1">
           <p
-            className={`width-auto welcome text text-size-header height-auto background-white flex-align-start text-align-center marginless split`}
+            className={`width-auto welcome text text-size-header height-auto background-white flex-align-start text-align-center marginless split `}
           >
             Welcome to ATL Brew!
           </p>
         </div>
-        <div className="text-body-container grid-row-start-2 grid-display grid-col-2">
-          <p
-            className={`padding-full height-100prcnt box-shadow background-green grid-row-start-2 text-align-center margin-right text-body`}
-          >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu
-            lobortis elementum nibh tellus. Massa sapien faucibus et molestie
-            ac. Duis ut diam quam nulla porttitor massa id neque. Arcu risus
-            quis varius quam quisque id. In pellentesque massa placerat duis
-            ultricies lacus sed turpis. Sit amet volutpat consequat mauris nunc
-            congue. Id neque aliquam vestibulum morbi. At elementum eu facilisis
-            sed. Sed viverra ipsum nunc aliquet bibendum enim facilisis gravida
-            neque. Mi in nulla posuere sollicitudin aliquam ultrices sagittis
-            orci a. Eu turpis egestas pretium aenean pharetra magna ac placerat.
-            Ullamcorper malesuada proin libero nunc consequat interdum varius
-            sit. Vulputate dignissim suspendisse in est ante in nibh mauris
-            cursus. Sollicitudin nibh sit amet commodo nulla facilisi nullam
-            vehicula. Pulvinar mattis nunc sed blandit. Tortor aliquam nulla
-            facilisi cras fermentum odio. Cursus sit amet dictum sit amet justo.
-            Tellus elementum sagittis vitae et leo duis. Et netus et malesuada
-            fames ac turpis egestas maecenas pharetra. Feugiat in ante metus
-            dictum at. Nec feugiat nisl pretium fusce id velit. Ligula
-            ullamcorper malesuada proin libero. Arcu bibendum at varius vel
-            pharetra. Sem integer vitae justo eget magna fermentum iaculis eu.
-            Vestibulum sed arcu non odio euismod. Eu non diam phasellus
-            vestibulum lorem sed risus ultricies tristique. Nec sagittis aliquam
-            malesuada bibendum arcu vitae.
-          </p>
-          <p
-            className={`padding-full height-100prcnt box-shadow background-blue grid-row-start-2 text-align-center text-body`}
-          >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu
-            lobortis elementum nibh tellus. Massa sapien faucibus et molestie
-            ac. Duis ut diam quam nulla porttitor massa id neque. Arcu risus
-            quis varius quam quisque id. In pellentesque massa placerat duis
-            ultricies lacus sed turpis. Sit amet volutpat consequat mauris nunc
-            congue. Id neque aliquam vestibulum morbi. At elementum eu facilisis
-            sed. Sed viverra ipsum nunc aliquet bibendum enim facilisis gravida
-            neque. Mi in nulla posuere sollicitudin aliquam ultrices sagittis
-            orci a. Eu turpis egestas pretium aenean pharetra magna ac placerat.
-            Ullamcorper malesuada proin libero nunc consequat interdum varius
-            sit. Vulputate dignissim suspendisse in est ante in nibh mauris
-            cursus. Sollicitudin nibh sit amet commodo nulla facilisi nullam
-            vehicula. Pulvinar mattis nunc sed blandit. Tortor aliquam nulla
-            facilisi cras fermentum odio. Cursus sit amet dictum sit amet justo.
-            Tellus elementum sagittis vitae et leo duis. Et netus et malesuada
-            fames ac turpis egestas maecenas pharetra. Feugiat in ante metus
-            dictum at. Nec feugiat nisl pretium fusce id velit. Ligula
-            ullamcorper malesuada proin libero. Arcu bibendum at varius vel
-            pharetra. Sem integer vitae justo eget magna fermentum iaculis eu.
-            Vestibulum sed arcu non odio euismod. Eu non diam phasellus
-            vestibulum lorem sed risus ultricies tristique. Nec sagittis aliquam
-            malesuada bibendum arcu vitae.
-          </p>
-        </div>
       </div>
       <div
-        className={`grid-col-start-2 flex-display flex-row justify-center align-center margin-full`}
+        className={`grid-col-start-2 flex-display flex-row justify-center align-center margin-full space`}
       ></div>
       <div
         className={`grid-col-start-2 flex-display flex-row justify-center align-center margin-bottom`}
